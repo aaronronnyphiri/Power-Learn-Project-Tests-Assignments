63560c193934b790fb9575998c8e80a4dcfd1fcf
AARON PHIRI <117758283+aaronronnyphiri@users.noreply.github.com>
2022-12-02 18:01:49 +0200

    Add files via upload

---
+++ Numbers.py
@@ -0,0 +1,10 @@
+#testing the output of tuple formating
+
+for num in range(3):
+    print(num)
+print("===============")
+for num in range(3,7):
+    print(num)
+print("===============")
+for num in range(3, 15, 4):
+    print(num)
